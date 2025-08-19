0d50166f2a83dc8390f5cae074641c0fec525d1c
Ramez Ragaay <113936321+RamezRagaay@users.noreply.github.com>
2025-08-19 16:06:59 +0300

    feat : HelloWorld API

---
+++ src/test/java/com/example/sprint2_task1/Sprint2Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprint2_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2Task1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
